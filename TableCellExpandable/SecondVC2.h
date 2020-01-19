2a7c14de54fd477cbb7d3910d328c9ac73cc8b9e
pravin parmar <pravinmarwadi@gmail.com>
2020-01-19 13:06:42 +0530

    take update

---
+++ TableCellExpandable/SecondVC2.h
@@ -0,0 +1,17 @@
+//
+//  SecondVC2.h
+//  TableCellExpandable
+//
+//  Created by Megha on 19/01/20.
+//  Copyright © 2020 Megha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondVC2 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TableCellExpandable/SecondVC2.m
@@ -0,0 +1,32 @@
+//
+//  SecondVC2.m
+//  TableCellExpandable
+//
+//  Created by Megha on 19/01/20.
+//  Copyright © 2020 Megha. All rights reserved.
+//
+
+#import "SecondVC2.h"
+
+@interface SecondVC2 ()
+
+@end
+
+@implementation SecondVC2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
